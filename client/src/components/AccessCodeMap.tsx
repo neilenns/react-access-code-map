d81cac1568e1af0d9701228fa67ce38399899a2b
Neil <neile@live.com>
2023-07-06 09:08:34 -0700

    Logout button is better, but state colours are inverted just like in the sample

--- client/src/components/AccessCodeMap.tsx
+++ client/src/components/AccessCodeMap.tsx
@@ -1,34 +1,40 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer } from "react-leaflet";
 import { LocateControl } from "./LocateControl";
-import { GeocodeControl } from './GeocodeControl';
-import LocationMarkers from './LocationMarkers';
-import Control from 'react-leaflet-custom-control';
-import { Button, Icon } from '@blueprintjs/core';
+import { GeocodeControl } from "./GeocodeControl";
+import LocationMarkers from "./LocationMarkers";
+import Control from "react-leaflet-custom-control";
+import { Button, ButtonGroup, Tooltip } from "@mui/material";
+import { ExitToApp } from "@mui/icons-material";
 
 export interface IAccessCodeMapProps {
-	onSignOutClick: React.MouseEventHandler;
+  onSignOutClick: React.MouseEventHandler;
 }
 
-export default function AccessCodeMap(props : IAccessCodeMapProps) {
-	const { onSignOutClick } = props;
+export default function AccessCodeMap(props: IAccessCodeMapProps) {
+  const { onSignOutClick } = props;
 
-	return (
-		<MapContainer 
-			center={[47.65496185820956, -122.25201847353225]} 
-			zoom={11} 
-			scrollWheelZoom={true}>
-			<TileLayer
-				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			/>
-			<GeocodeControl position="topright"/>
-			<LocateControl position="topright" />
-			<LocationMarkers/>
-			<Control position='topright'>
-				<Button onClick={onSignOutClick}>
-					<Icon icon="log-out"/>
-				</Button>
-			</Control>
-		</MapContainer>
-	);
+  return (
+    <MapContainer
+      center={[47.65496185820956, -122.25201847353225]}
+      zoom={11}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <GeocodeControl position="topright" />
+      <LocateControl position="topright" />
+      <LocationMarkers />
+      <Control position="bottomleft">
+        <ButtonGroup orientation="vertical" variant="contained">
+          <Tooltip placement="right" title="Sign Out">
+            <Button color="inherit" onClick={onSignOutClick}>
+              <ExitToApp />
+            </Button>
+          </Tooltip>
+        </ButtonGroup>
+      </Control>
+    </MapContainer>
+  );
 }
