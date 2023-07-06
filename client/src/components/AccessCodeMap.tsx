1204b96771137e0ef414fc273b9b5991795e64e2
Neil <neile@live.com>
2023-07-06 09:52:49 -0700

    Switch to using a different geocodecontrol

--- client/src/components/AccessCodeMap.tsx
+++ client/src/components/AccessCodeMap.tsx
@@ -23,7 +23,7 @@ export default function AccessCodeMap(props: IAccessCodeMapProps) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <GeocodeControl position="topright" />
+      <GeocodeControl />
       <LocateControl position="topright" />
       <LocationMarkers />
       <Control position="bottomleft">
--- client/src/components/GeocodeControl.tsx
+++ client/src/components/GeocodeControl.tsx
@@ -1,15 +1,28 @@
-import { createControlComponent } from '@react-leaflet/core';
-import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
-import 'leaflet-control-geocoder/dist/Control.Geocoder.js';
-
-import Geocoder from 'leaflet-control-geocoder'
+import { createControlComponent } from "@react-leaflet/core";
+import "leaflet-geosearch/dist/geosearch.css";
+import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
 
 // This code is based on https://stackoverflow.com/a/75567918.
 // Accessing the control is from https://github.com/perliedman/leaflet-control-geocoder/issues/260#issuecomment-1272343753
 interface P extends L.ControlOptions {}
 
 function createGeocodeInstance(props: P) {
-  const instance = new Geocoder(props);
+  // Unfortunate to have to add this ignore but otherwise typescript doesn't like the control.
+  // I found a sample on the leaflet-geosearch page that did this too so I guess
+  // that's how it has to be done.
+  // @ts-ignore
+  const instance = new GeoSearchControl({
+    provider: new OpenStreetMapProvider({
+      params: {
+        countrycodes: "us",
+      },
+    }),
+    style: "bar",
+    showMarker: false,
+    autoClose: true,
+    keepResult: true,
+    ...props,
+  });
 
   return instance;
 }
