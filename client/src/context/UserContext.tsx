372f29f64ed2093450cebe2afd6073ddb61ee328
Neil <neile@live.com>
2023-07-06 17:48:32 -0700

    Doesn't show edit or delete icons unless the user has permission

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -1,31 +1,34 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import { IUser } from "../interfaces/IUser.mjs";
 
 type IUserState = {
-    token?: string | null;
+  token?: string | null;
+  details?: IUser | null;
 };
 
 // Required so UserProvider doesn't throw a type error
 export interface IProviderProps {
-    children?: any;
-    defaultState?: IUserState;
+  children?: any;
+  defaultState?: IUserState;
 }
 
 // It took forever to find the way to get this to work without any type errors.
 // Solution comes from https://stackoverflow.com/a/62932958.
 // Minor modifications based on https://stackoverflow.com/a/68851743/9206264 as well.
 
-type IUserContext = [IUserState, React.Dispatch<React.SetStateAction<IUserState>>];
+type IUserContext = [
+  IUserState,
+  React.Dispatch<React.SetStateAction<IUserState>>
+];
 
-const UserContext = React.createContext<IUserContext>([{}, () => null])
+const UserContext = React.createContext<IUserContext>([{}, () => null]);
 
 const UserProvider = (props: IProviderProps) => {
-    const state = useState<IUserState>({...props.defaultState});
+  const state = useState<IUserState>({ ...props.defaultState });
 
-    return (
-        <UserContext.Provider value={state}>
-            {props.children}
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider value={state}>{props.children}</UserContext.Provider>
+  );
 };
 
-export { UserContext, UserProvider }
+export { UserContext, UserProvider };
