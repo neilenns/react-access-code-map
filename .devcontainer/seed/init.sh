5b551f1814641981d679953312e78c6d189ece3a
Neil Enns <neile@live.com>
2025-05-12 03:45:53 -0700

    Add a devcontainer (#185)
    * Add a devcontainer
    Fixes #182
    
    * Fix path

---
+++ .devcontainer/seed/init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "📦 Seeding database..."
+
+# Initialize the database
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export DB_URI="mongodb://db:27017/clearancelab"
+
+mongosh "${DB_URI}" "${SCRIPT_DIR}/init.js" > /dev/null
+
+mongoimport --quiet --uri="${DB_URI}" --collection=locations --file="${SCRIPT_DIR}/access-code-map.locations.json" --jsonArray
