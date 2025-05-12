c432766d49bb87cae241172eb8076322e700d491
Neil Enns <neile@live.com>
2025-05-12 08:59:40 -0700

    Seed correct db name, add task for it (#191)

--- .devcontainer/seed/init.sh
+++ .devcontainer/seed/init.sh
@@ -6,7 +6,7 @@ echo "📦 Seeding database..."
 # Initialize the database
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-export DB_URI="mongodb://db:27017/clearancelab"
+export DB_URI="mongodb://db:27017/access-code-map"
 
 mongosh "${DB_URI}" "${SCRIPT_DIR}/init.js" > /dev/null
 
